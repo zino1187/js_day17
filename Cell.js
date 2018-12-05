b2c6ae8c722f74cdc85f20a66e17d4543439d784
zino1187 <zino1187@gmail.com>
2018-12-05 11:51:07 +0900

    no message

---
+++ Cell.js
@@ -0,0 +1,20 @@
+/*달력을 구성하는 하나의 날짜 사각형을 정의한다*/
+class Cell{
+	constructor(container,width,height,bg,text){
+		this.container=container;
+		this.width=width;
+		this.height=height;
+		this.bg=bg;
+		this.text=text;
+
+		this.div=document.createElement("div");
+		this.div.style.width=this.width+"px";
+		this.div.style.height=this.height+"px";
+		this.div.style.border=1+"px solid blue";
+		this.div.style.background=this.bg;
+		this.div.innerText=this.text;
+		this.div.style.float="left";
+
+		this.container.appendChild(this.div);
+	}
+}
